b3b5fb9194a810623e5b01fa63a480fe6ebb89e3
Bezzz <xeightfour@gmail.com>
2024-07-24 01:20:58 +0330

    Refactoring

--- install/install.sh
+++ install/install.sh
@@ -4,98 +4,99 @@ SRC=$(cd ../ && pwd)/src
 CFG=~/.config
 LCL=~/.local
 
-# Proccess assets
-echo '-> Copying font files'
+# Process assets
+echo "-> Copying font files"
 mkdir -p $LCL/share/fonts
 cp --force $SRC/assets/fonts/SFMono/* $LCL/share/fonts
 cp --force $SRC/assets/fonts/Iranian/* $LCL/share/fonts
 cp --force $SRC/assets/fonts/SXMono/* $LCL/share/fonts
 
-# Setting up bash
-echo '-> Linking bash configuration'
-ln -sf $SRC/config/bash/bashrc ~/bashrc
-if ! grep -Fq 'source ~/bashrc' ~/.bashrc; then
-    echo -e '\n[[ -f ~/bashrc ]] && source ~/bashrc' >> ~/.bashrc
-fi
+# Set up bash configuration
 if [[ ! -f ~/git-prompt.sh ]]; then
-    echo '-> Downloading git-prompt bash script'
-    curl -so ~/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh || echo '[ERROR] Failed to download git-prompt'
+    echo "-> Downloading git-prompt bash script"
+    curl -so ~/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh || echo "[ERROR] Failed to download git-prompt"
 fi
-echo '-> Linking inputrc'
+echo "-> Linking bash configuration"
+ln -sf $SRC/config/bash/bashrc ~/bashrc
+if ! grep -Fq "source ~/bashrc" ~/.bashrc; then
+    echo -e "\n[[ -f ~/bashrc ]] && source ~/bashrc" >> ~/.bashrc
+fi
+
+echo "-> Linking inputrc"
 ln -sf $SRC/config/bash/inputrc ~/.inputrc
 
-# Linking fontconfig
-echo '-> Linking fontconfig'
+# Link fontconfig
+echo "-> Linking fontconfig"
 [[ -f $CFG/fontconfig ]] && rm -f $CFG/fontconfig
 [[ -d $CFG/fontconfig ]] && rm -Rf $CFG/fontconfig
 ln -sf $SRC/config/fontconfig $CFG/fontconfig
 
-# Linking alacritty configuration
-echo '-> Linking alacritty configuration'
+# Link alacritty configuration
+echo "-> Linking alacritty configuration"
 [[ -f $CFG/alacritty ]] && rm -f $CFG/alacritty
 [[ -d $CFG/alacritty ]] && rm -Rf $CFG/alacritty
 ln -sf $SRC/config/alacritty $CFG/alacritty
 
-# Linking GTK-3.0 configuration
-echo '-> Linking GTK-3.0 configuration'
+# Link GTK-3.0 configuration
+echo "-> Linking GTK-3.0 configuration"
 [[ -f $CFG/gtk-3.0 ]] && rm -f $CFG/gtk-3.0
 [[ -d $CFG/gtk-3.0 ]] && rm -Rf $CFG/gtk-3.0
 ln -sf $SRC/config/gtk-3.0 $CFG/gtk-3.0
 
-# Linking i3 configuration
-echo '-> Linking i3 configuration'
+# Link i3 configuration
+echo "-> Linking i3 configuration"
 [[ -f $CFG/i3 ]] && rm -f $CFG/i3
 [[ -d $CFG/i3 ]] && rm -Rf $CFG/i3
 ln -sf $SRC/config/i3 $CFG/i3
 
-# Linking picom configuration
-echo '-> Linking picom configuration'
+# Link picom configuration
+echo "-> Linking picom configuration"
 [[ -f $CFG/picom ]] && rm -f $CFG/picom
 [[ -d $CFG/picom ]] && rm -Rf $CFG/picom
 ln -sf $SRC/config/picom $CFG/picom
 
-# Linking tmux configuration
-echo '-> Linking tmux configuration'
+# Link tmux configuration
+echo "-> Linking tmux configuration"
 [[ -f $CFG/tmux ]] && rm -f $CFG/tmux
 [[ -d $CFG/tmux ]] && rm -Rf $CFG/tmux
 ln -sf $SRC/config/tmux $CFG/tmux
 
-# Linking neovim configuration
-echo '-> Linking neovim configuration'
+# Link neovim configuration
+echo "-> Linking neovim configuration"
 [[ -f $CFG/nvim ]] && rm -f $CFG/nvim
 [[ -d $CFG/nvim ]] && rm -Rf $CFG/nvim
 ln -sf $SRC/config/nvim $CFG/nvim
 
-# Installing neovim plugins
-echo '-> Installing neovim plugins'
+# Install neovim plugins
+echo "-> Installing neovim plugins"
 NEOVIM=$LCL/share/nvim/site/pack/bezzz/start
 mkdir -p $NEOVIM
 if [[ -d $NEOVIM/catppuccin ]]; then
-    read -p ':: A version of catppuccin theme is already installed, do you wish to update it? (*YES*/NO): ' confirm
+    read -p ":: A version of catppuccin theme is already installed, do you wish to update it? (*YES*/NO): " confirm
     if ! [[ $confirm == [nN] || $confirm == [nN][oO] ]]; then
         rm -rf $NEOVIM/catppuccin
-        echo '-> Downloading catppuccin'
-        git clone -q https://github.com/catppuccin/nvim.git $NEOVIM/catppuccin || echo '[ERROR] Failed to download catppuccin'
+        echo "-> Downloading catppuccin"
+        git clone -q https://github.com/catppuccin/nvim.git $NEOVIM/catppuccin || echo "[ERROR] Failed to download catppuccin"
     else
-        echo ':: Ok, ignoring catppuccin'
+        echo ":: Ok, ignoring catppuccin"
     fi
 else
-    echo '-> Downloading catppuccin'
-    git clone -q https://github.com/catppuccin/nvim.git $NEOVIM/catppuccin || echo '[ERROR] Failed to download catppuccin'
+    echo "-> Downloading catppuccin"
+    git clone -q https://github.com/catppuccin/nvim.git $NEOVIM/catppuccin || echo "[ERROR] Failed to download catppuccin"
 fi
 if [[ -d $NEOVIM/vim-cpp-modern ]]; then
-    read -p ':: A version of vim-cpp-modern is already installed, do you wish to update it? (*YES*/NO): ' confirm
+    read -p ":: A version of vim-cpp-modern is already installed, do you wish to update it? (*YES*/NO): " confirm
     if ! [[ $confirm == [nN] || $confirm == [nN][oO] ]]; then
         rm -rf $NEOVIM/vim-cpp-modern
-        echo '-> Downloading vim-cpp-modern'
-        git clone -q https://github.com/bfrg/vim-cpp-modern.git $NEOVIM/vim-cpp-modern || echo '[ERROR] Failed to download vim-cpp-modern'
+        echo "-> Downloading vim-cpp-modern"
+        git clone -q https://github.com/bfrg/vim-cpp-modern.git $NEOVIM/vim-cpp-modern || echo "[ERROR] Failed to download vim-cpp-modern"
     else
-        echo ':: Ok, ignoring vim-cpp-modern'
+        echo ":: Ok, ignoring vim-cpp-modern"
     fi
 else
-    echo '-> Downloading vim-cpp-modern'
-    git clone -q https://github.com/bfrg/vim-cpp-modern.git $NEOVIM/vim-cpp-modern || echo '[ERROR] Failed to download vim-cpp-modern'
+    echo "-> Downloading vim-cpp-modern"
+    git clone -q https://github.com/bfrg/vim-cpp-modern.git $NEOVIM/vim-cpp-modern || echo "[ERROR] Failed to download vim-cpp-modern"
 fi
 
 # Post installation
-echo ':: All set'
+echo ":: All set"
--- src/scripts/freak.cxx
+++ src/scripts/freak.cxx
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <fstream>
 
-using namespace std;
-
-#define MAX_FILE_SIZE 128'000'000
+constexpr MAX_FILE_SIZE = 128'000'000;
 
 char content[MAX_FILE_SIZE], c;
 
 int main(int argc, char **args) {
     for (int i = 1; i < argc; i++) {
         const char *fileName = args[i];
-        ifstream inFile(fileName, ios::in | ios::binary);
+        std::ifstream inFile(fileName, std::ios::in | std::ios::binary);
         if (!inFile.is_open()) {
             cout << "Failed to open " << fileName << " ):" << endl;
             continue;
@@ -20,8 +18,8 @@ int main(int argc, char **args) {
             content[x++] = c;
         }
         inFile.close();
-        cout << x << " Bytes" << endl;
-        ofstream outFile(fileName, ios::out | ios::binary);
+        std::cout << "Processed " << x << " Bytes" << std::endl;
+        std::ofstream outFile(fileName, std::ios::out | std::ios::binary);
         int y = 0;
         while (y < x) {
             outFile << noskipws << (char)(content[y++] ^ 'x');
