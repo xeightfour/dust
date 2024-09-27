d0a782abd7df4aa6d33f731e317071237615ad50
ooosahmad <xeightfour@gmail.com>
2024-09-27 07:18:09 +0330

    Remove picom from laptop

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ sudo pacman -S "${inst[@]}" || bounce
 
 # Get git-prompt.sh
 if ! [[ -f ~/git-prompt.sh ]]; then
-	echo ':: Receiving git-prompt.sh'
+	echo 'Receiving git-prompt.sh...'
 	curl -so ~/git-prompt.sh 'https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh' || bounce
 fi
 
