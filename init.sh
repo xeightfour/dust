7ce667bca2321635ad01e4711a9b0b5ef5b2a57b
ooosahmad <xeightfour@gmail.com>
2025-01-02 22:16:40 +0330

    Minor

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Just in case
+dust=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") &> /dev/null && pwd)
+
+function bounce {
+	echo '[ERROR] donno what happened >:' && exit 1
+}
+
+if ! [[ -f ~/git-prompt.sh ]]; then
+	echo 'Receiving git-prompt.sh...'
+	curl -so ~/git-prompt.sh 'https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh' || bounce
+fi
+
+echo 'All done <:'
