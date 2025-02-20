df366c8b00c38fc4bd1a2ef2f7ee027cc9ea812c
ooosahmad <xeightfour@gmail.com>
2025-02-20 19:55:25 +0330

    Better setup script
    Increased gtk font size to 12
    Removed minimum window size setting for i3
    Setup now resets values before applying, to ensure that kernel updates them

--- scripts/setup.hi.sh
+++ scripts/setup.hi.sh
@@ -6,8 +6,21 @@ if [[ $# -eq 1 ]] && [[ $1 -eq 1 ]]; then
 	rfkill block bluetooth
 fi
 
-# CPU
+# Reset values
+echo Resetting values...
 echo balance_performance | tee /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference
+echo 800000 | tee /sys/devices/system/cpu/cpufreq/policy*/scaling_min_freq
+echo 2000000 | tee /sys/devices/system/cpu/cpufreq/policy*/scaling_max_freq
+echo 0 | tee /sys/devices/system/cpu/cpufreq/boost
+echo auto | tee /sys/class/drm/card*/device/power_dpm_force_performance_level
+echo balanced | tee /sys/class/drm/card*/device/power_dpm_state
+echo 3 | tee /sys/class/drm/card*/device/pp_power_profile_mode
+echo 1 | tee /sys/class/thermal/thermal_zone0/cdev*/cur_state
+sleep 1.6
+
+echo Applying new settings...
+# CPU
+echo balance_power | tee /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference
 echo 400000 | tee /sys/devices/system/cpu/cpufreq/policy*/scaling_min_freq
 echo 2500000 | tee /sys/devices/system/cpu/cpufreq/policy*/scaling_max_freq
 echo 1 | tee /sys/devices/system/cpu/cpufreq/boost
@@ -15,6 +28,7 @@ echo 1 | tee /sys/devices/system/cpu/cpufreq/boost
 # GPU
 echo low | tee /sys/class/drm/card*/device/power_dpm_force_performance_level
 echo battery | tee /sys/class/drm/card*/device/power_dpm_state
+echo 1 | tee /sys/class/drm/card*/device/pp_power_profile_mode
 
 # CPU & GPU
 echo 3 | tee /sys/class/thermal/thermal_zone0/cdev*/cur_state
--- scripts/setup.lo.sh
+++ scripts/setup.lo.sh
@@ -6,10 +6,23 @@ if [[ $# -eq 1 ]] && [[ $1 -eq 1 ]]; then
 	rfkill block bluetooth
 fi
 
+# Reset values
+echo Resetting values...
+echo balance_performance | tee /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference
+echo 800000 | tee /sys/devices/system/cpu/cpufreq/policy*/scaling_min_freq
+echo 2000000 | tee /sys/devices/system/cpu/cpufreq/policy*/scaling_max_freq
+echo 1 | tee /sys/devices/system/cpu/cpufreq/boost
+echo auto | tee /sys/class/drm/card*/device/power_dpm_force_performance_level
+echo balanced | tee /sys/class/drm/card*/device/power_dpm_state
+echo 3 | tee /sys/class/drm/card*/device/pp_power_profile_mode
+echo 1 | tee /sys/class/thermal/thermal_zone0/cdev*/cur_state
+sleep 1.6
+
+echo Applying new settings...
 # CPU
 echo power | tee /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference
 echo 400000 | tee /sys/devices/system/cpu/cpufreq/policy*/scaling_min_freq
-echo 1800000 | tee /sys/devices/system/cpu/cpufreq/policy*/scaling_max_freq
+echo 1600000 | tee /sys/devices/system/cpu/cpufreq/policy*/scaling_max_freq
 echo 0 | tee /sys/devices/system/cpu/cpufreq/boost
 
 # GPU
