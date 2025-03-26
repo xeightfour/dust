e4643866a391e67cc965ee94aeaadf5d8710a09e
soheil-photon <xeightfour@gmail.com>
2025-03-26 19:06:03 +0330

    Initial photon commit

--- init.sh
+++ init.sh
@@ -2,36 +2,19 @@
 
 set -e
 
-dust="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)" || {
-	echo '[ERROR] Failed to determine script directory >=' >&2
+bounce() {
+	echo "$1" >&2
 	exit 1
 }
 
-# Download git-prompt.sh if it doesn't exist
-if ! [[ -f ~/git-prompt.sh ]]; then
-	echo 'Downloading git-prompt.sh...'
-	curl -so ~/git-prompt.sh 'https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh' || {
-		echo '[ERROR] Failed to download git-prompt.sh >=' >&2
-		exit 1
-	}
-fi
+dust="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 scripts="$dust/scripts"
-if [[ -d $scripts ]]; then
+if [[ -d "$scripts" ]]; then
 	echo "Making scripts in $scripts executable..."
-	chmod +x "$scripts"/* || {
-		echo '[ERROR] Failed to make scripts executable >=' >&2
-		exit 1
-	}
+	chmod +x "$scripts"/* || bounce 'Failed to make scripts executable >:'
 else
-	echo "[ERROR] Directory not found: $scripts >=" >&2
-	exit 1
+	bounce "Directory not found: $scripts >:"
 fi
-
-echo 'Running dset script...'
-"$scripts/dset" || {
-	echo '[ERROR] dset script failed >=' >&2
-	exit 1
-}
 
 echo 'Initialization was successful <:'
