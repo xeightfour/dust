fc9e46e4af04837951336290e3686506336c05c6
soheil-photon <xeightfour@gmail.com>
2025-03-29 15:55:11 +0330

    Format Qutebrowser `config.py`
    Reformatted quotes (single -> double where appropriate) for consistency
    Adjust whitespace in dictionaries and lists (e.g., `{"key": val}`)
    Break long lines to 79-char limit (PEP 8 compliance)
    Standardize string concatenation in download commands
    No functional changes; purely stylistic improvements

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -5,11 +5,11 @@ config.load_autoconfig(True)
 
 catppuccin.setup(c, "mocha", True)
 
-homeDir = os.path.expanduser('~')
-downDir = os.path.join(homeDir, 'Downloads')
+homeDir = os.path.expanduser("~")
+downDir = os.path.join(homeDir, "Downloads")
 
 config.set("zoom.default", "125%")
-config.set("statusbar.padding", { "bottom": 2, "left": 0, "right": 0, "top": 0 })
+config.set("statusbar.padding", {"bottom": 2, "left": 0, "right": 0, "top": 0})
 
 # Tabs
 config.set("tabs.position", "right")
@@ -31,11 +31,16 @@ config.set("auto_save.session", True)
 config.set("session.lazy_restore", True)
 
 # Search engines
-config.set("url.searchengines", {
-    "DEFAULT": "https://www.duckduckgo.com/search?q={}",
-    "g": "https://www.google.com/search?q={}",
-    "pkg": "https://archlinux.org/packages/?sort=&q={}&maintainer=&flagged="
-})
+config.set(
+    "url.searchengines",
+    {
+        "DEFAULT": "https://www.duckduckgo.com/search?q={}",
+        "g": "https://www.google.com/search?q={}",
+        "pkg": (
+            "https://archlinux.org/packages/?sort=&q={}&maintainer=&flagged="
+        ),
+    },
+)
 
 # Fonts
 config.set("fonts.default_size", "13pt")
@@ -50,14 +55,34 @@ config.set("fonts.completion.category", "13pt Monolisa Trial")
 config.bind("<space>Q", "quit")
 config.bind("<space>r", "config-source ;; message-info 'Reloaded!'")
 config.bind("<space><space>", "clear-messages")
-config.bind("<space>m", "spawn bash -c \"yt-dlp --cookies-from-browser firefox --limit-rate 70K -f 18 -o - {url} | mpv -\"")
-config.bind("<space>d", "spawn bash -c \"yt-dlp --cookies-from-browser firefox -f 18 -o '" + downDir + "/%(title)s.%(ext)s' '{url}'\"")
-config.bind("<space>h", "spawn bash -c \"yt-dlp --cookies-from-browser firefox -f 'bestvideo[height<=720]+bestaudio/best[height<=720]' -o '" + downDir + "/%(title)s.%(ext)s' '{url}'\"")
-config.bind("<space>a", "spawn bash -c \"yt-dlp --cookies-from-browser firefox -f 140/250 -o '" + downDir + "/%(title)s.%(ext)s' '{url}'\"")
+config.bind(
+    "<space>m",
+    'spawn bash -c "yt-dlp --cookies-from-browser firefox --limit-rate 70K -f'
+    " 18 -o - '{url}' | mpv -\"",
+)
+config.bind(
+    "<space>d",
+    "spawn bash -c \"yt-dlp --cookies-from-browser firefox -f 18 -o '"
+    + downDir
+    + "/%(title)s.%(ext)s' '{url}'\"",
+)
+config.bind(
+    "<space>h",
+    'spawn bash -c "yt-dlp --cookies-from-browser firefox -f'
+    " 'bestvideo[height<=720]+bestaudio/best[height<=720]' -o '"
+    + downDir
+    + "/%(title)s.%(ext)s' '{url}'\"",
+)
+config.bind(
+    "<space>a",
+    "spawn bash -c \"yt-dlp --cookies-from-browser firefox -f 140/250 -o '"
+    + downDir
+    + "/%(title)s.%(ext)s' '{url}'\"",
+)
 config.bind("q", "nop")
 
 # Spell check
-config.set("spellcheck.languages", [ "en-US" ])
+config.set("spellcheck.languages", ["en-US"])
 
-# Brave adblock
+# Brave ad-block
 config.set("content.blocking.method", "both")
